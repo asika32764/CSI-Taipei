275559957c3271cbd33030f9b5feeb000744a5f8
Asika <asika32764@gmail.com>
2013-10-21 22:47:28 +0800

    assets include ok

--- index.php
+++ index.php
@@ -6,8 +6,20 @@ $config = require 'config.php';
 
 $app = new \Slim\Slim($config);
 
-$app->get('/:path+', function($path) use($app){
-    $app->render('abc.twig', array('path' => $path));
-});
+$execute = function($path) use($app)
+{
+    // Get assets path
+    $base = str_replace($_SERVER['DOCUMENT_ROOT'], '', dirname($_SERVER['SCRIPT_FILENAME']));
+    
+    $template = implode('/', $path);
+    
+    $template = $template ?: 'index';
+    
+    $app->render($template . '.twig', array('path' => $path, 'uri' => array('base' => $base)));
+};
+
+$app->get('/', $execute);
+
+$app->get('/:path+', $execute);
 
 $app->run();
