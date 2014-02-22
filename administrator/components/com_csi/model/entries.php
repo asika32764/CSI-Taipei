a9389983863d6bf118665f9543b134be66f73ce8
Simon Asika <asika32764@gmail.com>
2014-02-22 14:21:26 +0800

    Prepare development

---
+++ administrator/components/com_csi/model/entries.php
@@ -0,0 +1,95 @@
+<?php
+
+use Windwalker\DI\Container;
+use Windwalker\Model\Filter\FilterHelper;
+use Windwalker\Model\ListModel;
+
+/**
+ * Class CsiModelEntries
+ *
+ * @since 1.0
+ */
+class CsiModelEntries extends ListModel
+{
+	/**
+	 * configureTables
+	 *
+	 * @return  void
+	 */
+	protected function configureTables()
+	{
+		$queryHelper = $this->getContainer()->get('model.entries.helper.query', Container::FORCE_NEW);
+
+		$queryHelper->addTable('entry', '#__csi_entries')
+			->addTable('category',  '#__categories', 'entry.catid      = category.id')
+			->addTable('user',      '#__users',      'entry.created_by = user.id')
+			->addTable('viewlevel', '#__viewlevels', 'entry.access     = viewlevel.id')
+			->addTable('lang',      '#__languages',  'entry.language   = lang.lang_code');
+
+		$this->filterFields = array_merge($this->filterFields, $queryHelper->getFilterFields());
+	}
+
+	/**
+	 * populateState
+	 *
+	 * @param null $ordering
+	 * @param null $direction
+	 *
+	 * @return  void
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// Build ordering prefix
+		if (!$ordering)
+		{
+			$table = $this->getTable('Entry');
+
+			$ordering = property_exists($table, 'ordering') ? 'entry.ordering' : 'entry.id';
+
+			$ordering = property_exists($table, 'catid') ? 'entry.catid, ' . $ordering : $ordering;
+		}
+
+		parent::populateState($ordering, 'ASC');
+	}
+
+	/**
+	 * processFilters
+	 *
+	 * @param JDatabaseQuery $query
+	 * @param array          $filters
+	 *
+	 * @return  JDatabaseQuery
+	 */
+	protected function processFilters(\JDatabaseQuery $query, $filters = array())
+	{
+		// If no state filter, set published >= 0
+		if (!isset($filters['entry.published']) && property_exists($this->getTable(), 'published'))
+		{
+			$query->where($query->quoteName('entry.published') . ' >= 0');
+		}
+
+		return parent::processFilters($query, $filters);
+	}
+
+	/**
+	 * configureFilters
+	 *
+	 * @param FilterHelper $filterHelper
+	 *
+	 * @return  void
+	 */
+	protected function configureFilters($filterHelper)
+	{
+	}
+
+	/**
+	 * configureSearches
+	 *
+	 * @param \Windwalker\Model\Filter\SearchHelper $searchHelper
+	 *
+	 * @return  void
+	 */
+	protected function configureSearches($searchHelper)
+	{
+	}
+}
---
+++ administrator/components/com_csi/model/task.php
@@ -0,0 +1,24 @@
+<?php
+
+use Windwalker\Model\AdminModel;
+
+/**
+ * Class CsiModelTask
+ *
+ * @since 1.0
+ */
+class CsiModelTask extends AdminModel
+{
+	/**
+	 * Method to set new item ordering as first or last.
+	 *
+	 * @param   JTable $table    Item table to save.
+	 * @param   string $position 'first' or other are last.
+	 *
+	 * @return  void
+	 */
+	public function setOrderPosition($table, $position = 'last')
+	{
+		parent::setOrderPosition($table, $position);
+	}
+}
