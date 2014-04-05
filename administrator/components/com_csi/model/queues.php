3d0d494e647f1cb3713442a86948bc43b5358fae
Simon Asika <asika32764@gmail.com>
2014-04-05 13:30:16 +0800

    Add Queues

---
+++ administrator/components/com_csi/model/queues.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Part of Component Csi files.
+ *
+ * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Windwalker\DI\Container;
+use Windwalker\Model\Filter\FilterHelper;
+use Windwalker\Model\ListModel;
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Class CsiModelQueues
+ *
+ * @since 1.0
+ */
+class CsiModelQueues extends ListModel
+{
+	/**
+	 * Property prefix.
+	 *
+	 * @var  string
+	 */
+	protected $prefix = 'csi';
+
+	/**
+	 * Property option.
+	 *
+	 * @var  string
+	 */
+	protected $option = 'com_csi';
+
+	/**
+	 * Property textPrefix.
+	 *
+	 * @var string
+	 */
+	protected $textPrefix = 'COM_CSI';
+
+	/**
+	 * Property viewItem.
+	 *
+	 * @var  string
+	 */
+	protected $viewItem = 'queue';
+
+	/**
+	 * Property viewList.
+	 *
+	 * @var  string
+	 */
+	protected $viewList = 'queues';
+
+	/**
+	 * configureTables
+	 *
+	 * @return  void
+	 */
+	protected function configureTables()
+	{
+		$queryHelper = $this->getContainer()->get('model.queues.helper.query', Container::FORCE_NEW);
+
+		$queryHelper->addTable('queue', '#__csi_queues')
+			->addTable('category',  '#__categories', 'queue.catid      = category.id')
+			->addTable('user',      '#__users',      'queue.created_by = user.id')
+			->addTable('viewlevel', '#__viewlevels', 'queue.access     = viewlevel.id')
+			->addTable('lang',      '#__languages',  'queue.language   = lang.lang_code');
+
+		$this->filterFields = array_merge($this->filterFields, $queryHelper->getFilterFields());
+	}
+
+	/**
+	 * populateState
+	 *
+	 * @param null $ordering
+	 * @param null $direction
+	 *
+	 * @return  void
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// Build ordering prefix
+		if (!$ordering)
+		{
+			$table = $this->getTable('Queue');
+
+			$ordering = property_exists($table, 'ordering') ? 'queue.ordering' : 'queue.id';
+
+			$ordering = property_exists($table, 'catid') ? 'queue.catid, ' . $ordering : $ordering;
+		}
+
+		parent::populateState($ordering, 'ASC');
+	}
+
+	/**
+	 * processFilters
+	 *
+	 * @param JDatabaseQuery $query
+	 * @param array          $filters
+	 *
+	 * @return  JDatabaseQuery
+	 */
+	protected function processFilters(\JDatabaseQuery $query, $filters = array())
+	{
+		// If no state filter, set published >= 0
+		if (!isset($filters['queue.state']) && property_exists($this->getTable(), 'state'))
+		{
+			$query->where($query->quoteName('queue.state') . ' >= 0');
+		}
+
+		return parent::processFilters($query, $filters);
+	}
+
+	/**
+	 * configureFilters
+	 *
+	 * @param FilterHelper $filterHelper
+	 *
+	 * @return  void
+	 */
+	protected function configureFilters($filterHelper)
+	{
+	}
+
+	/**
+	 * configureSearches
+	 *
+	 * @param \Windwalker\Model\Filter\SearchHelper $searchHelper
+	 *
+	 * @return  void
+	 */
+	protected function configureSearches($searchHelper)
+	{
+	}
+}
