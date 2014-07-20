87edecb4fa3a771d9ecb5e670e5e83557b797b7f
Simon Asika <asika32764@gmail.com>
2014-07-20 20:37:53 +0800

    Entry view

--- administrator/components/com_csi/view/entries/tmpl/default_table.php
+++ administrator/components/com_csi/view/entries/tmpl/default_table.php
@@ -38,34 +38,14 @@ $grid->registerTableSort();
 <!-- TABLE HEADER -->
 <thead>
 <tr>
-	<!--SORT-->
-	<th width="1%" class="nowrap center hidden-phone">
-		<?php echo $grid->orderTitle(); ?>
-	</th>
-
 	<!--CHECKBOX-->
 	<th width="1%" class="center">
 		<?php echo JHtml::_('grid.checkAll'); ?>
 	</th>
 
-	<!--STATE-->
-	<th width="5%" class="nowrap center">
-		<?php echo $grid->sortTitle('JSTATUS', 'entry.state'); ?>
-	</th>
-
 	<!--TITLE-->
 	<th class="center">
 		<?php echo $grid->sortTitle('JGLOBAL_TITLE', 'entry.title'); ?>
-	</th>
-
-	<!--CATEGORY-->
-	<th width="10%" class="center">
-		<?php echo $grid->sortTitle('JCATEGORY', 'category.title'); ?>
-	</th>
-
-	<!--ACCESS VIEW LEVEL-->
-	<th width="5%" class="center">
-		<?php echo $grid->sortTitle('JGRID_HEADING_ACCESS', 'viewlevel.title'); ?>
 	</th>
 
 	<!--CREATED-->
@@ -76,11 +56,6 @@ $grid->registerTableSort();
 	<!--USER-->
 	<th width="10%" class="center">
 		<?php echo $grid->sortTitle('JAUTHOR', 'user.name'); ?>
-	</th>
-
-	<!--LANGUAGE-->
-	<th width="5%" class="center">
-		<?php echo $grid->sortTitle('JGRID_HEADING_LANGUAGE', 'lang.title'); ?>
 	</th>
 
 	<!--ID-->
@@ -112,25 +87,10 @@ $grid->registerTableSort();
 	$grid->setItem($item, $i);
 	?>
 	<tr class="entry-row" sortable-group-id="<?php echo $item->catid; ?>">
-		<!-- DRAG SORT -->
-		<td class="order nowrap center hidden-phone">
-			<?php echo $grid->dragSort(); ?>
-		</td>
 
 		<!--CHECKBOX-->
 		<td class="center">
 			<?php echo JHtml::_('grid.id', $i, $item->entry_id); ?>
-		</td>
-
-		<!--STATE-->
-		<td class="center">
-			<div class="btn-group">
-				<!-- STATE BUTTON -->
-				<?php echo $grid->state() ?>
-
-				<!-- CHANGE STATE DROP DOWN -->
-				<?php echo $this->loadTemplate('dropdown'); ?>
-			</div>
 		</td>
 
 		<!--TITLE-->
@@ -142,45 +102,16 @@ $grid->registerTableSort();
 				<!-- Title -->
 				<?php echo $grid->editTitle(); ?>
 			</div>
-
-			<!-- Sub Title -->
-			<div class="small">
-				<?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
-			</div>
-		</td>
-
-		<!--CATEGORY-->
-		<td class="center">
-			<?php echo $this->escape($item->category_title); ?>
-		</td>
-
-		<!--ACCESS VIEW LEVEL-->
-		<td class="center">
-			<?php echo $this->escape($item->viewlevel_title); ?>
 		</td>
 
 		<!--CREATED-->
 		<td class="center">
-			<?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+			<?php echo JHtml::_('date', $item->created, 'Y-m-d H:i:s'); ?>
 		</td>
 
 		<!--USER-->
 		<td class="center">
 			<?php echo $this->escape($item->user_name); ?>
-		</td>
-
-		<!--LANGUAGE-->
-		<td class="center">
-			<?php
-			if ($item->language == '*')
-			{
-				echo JText::alt('JALL', 'language');
-			}
-			else
-			{
-				echo $item->lang_title ? $this->escape($item->lang_title) : JText::_('JUNDEFINED');
-			}
-			?>
 		</td>
 
 		<!--ID-->
