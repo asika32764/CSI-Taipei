22cfb8cd705760a767847ca3c800389f92a691be
Simon Asika <asika32764@gmail.com>
2014-12-10 23:02:56 +0800

    Basic console & seeder

---
+++ sms/src/Csi/Seed/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Part of csi project. 
+ *
+ * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Csi\Seed;
+
+use SMS\Seeder\AbstractSeeder;
+
+/**
+ * The DatabaseSeeder class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class DatabaseSeeder extends AbstractSeeder
+{
+	/**
+	 * doExecute
+	 *
+	 * @return  void
+	 */
+	public function doExecute()
+	{
+
+	}
+
+	/**
+	 * doClean
+	 *
+	 * @return  void
+	 */
+	public function doClean()
+	{
+
+	}
+}
+ 
