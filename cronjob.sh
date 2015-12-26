d14008e8920783f08c71cf992e6af73f47d1e1e7
Simon Asika <asika32764@gmail.com>
2015-12-26 22:04:02 +0800

    cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget -O /dev/null http://localhost/csi/?option=com_csi&task=queue.execute
+wget -O /dev/null http://localhost/csi/?task=queue.execute
 
 sleep 20s
 
-wget -O /dev/null http://localhost/csi/?option=com_csi&task=queue.execute
+wget -O /dev/null http://localhost/csi/?task=queue.execute
 
 sleep 20s
 
-wget -O /dev/null http://localhost/csi/?option=com_csi&task=queue.execute
+wget -O /dev/null http://localhost/csi/?task=queue.execute
